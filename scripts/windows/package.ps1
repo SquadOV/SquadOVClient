9df2ba255a5f6b1a4799552b0a97aa2d9d1c5def
Michael Bao <mike.h.bao@gmail.com>
2020-10-03 19:43:26 -0400

    build and publish script

--- scripts/windows/package.ps1
+++ scripts/windows/package.ps1
@@ -1,5 +1,12 @@
 Write-Host "Begin Packaging Windows SquadOV"
 
+Set-Location -Path ..\..
+cmake -S . -B build -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release --target squadov_client_service
 
+Set-Location -Path .\client_ui
+npm run prodbuild
+npm run dist -- --config ..\scripts\windows\electron-builder.yml --publish always
 
+Set-Location -Path ..\scripts\windows
 Write-Host "Finish Packinag Windows SquadOV"
