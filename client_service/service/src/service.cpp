189b665abf8d7caf68e59a8b3dc78f2fca2a7dcf
Michael Bao <mike.h.bao@gmail.com>
2020-10-06 11:10:11 -0400

    handle top level errors and log it

--- client_service/service/src/service.cpp
+++ client_service/service/src/service.cpp
@@ -5,7 +5,9 @@
 #include "valorant/valorant_process_handler.h"
 #include "database/api.h"
 #include "shared/errors/error.h"
+#include "shared/log/log.h"
 
+#include <boost/stacktrace.hpp>
 #include <chrono>
 #include <cstdio>
 #include <curl/curl.h>
@@ -18,11 +20,27 @@ extern "C" {
 #include <libavutil/log.h>
 }
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 namespace fs = std::filesystem;
 
 process_watcher::ProcessWatcher watcher;
 
-void ffmpeg_log_callback(void* ptr, int level, const char* fmt, va_list v1) {
+#ifdef _WIN32
+
+LONG handleTopLevelExceptions(EXCEPTION_POINTERS* ex) {
+    LOG_ERROR(
+        "SquadOV Fatal Error [Code: " << ex->ExceptionRecord->ExceptionCode << "]" << std::endl
+            << boost::stacktrace::stacktrace() << std::endl
+    );
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+#endif
+
+void ffmpegLogCallback(void* ptr, int level, const char* fmt, va_list v1) {
     if (level > av_log_get_level()) {
         return;
     }
@@ -30,8 +48,12 @@ void ffmpeg_log_callback(void* ptr, int level, const char* fmt, va_list v1) {
     fflush(stdout);
 }
 
-
 int main(int argc, char** argv) {
+
+#ifdef _WIN32
+    SetUnhandledExceptionFilter(handleTopLevelExceptions);
+#endif
+
     if (curl_global_init(CURL_GLOBAL_ALL)) {
         THROW_ERROR("Failed to initialize CURL.");
     }
@@ -41,7 +63,7 @@ int main(int argc, char** argv) {
     }
 
     // Init FFmpeg logging - not sure why the default ffmpeg logging isn't working.
-    av_log_set_callback(ffmpeg_log_callback);
+    av_log_set_callback(ffmpegLogCallback);
 
     // Connect to the database which should be in the predefined spot in the APPDATA (or its OS equivalent) folder.
     const fs::path dbFname = shared::filesystem::getSquadOvFolder() / fs::path("squadov.db");
