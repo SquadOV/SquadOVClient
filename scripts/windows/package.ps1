e7300d39d05eba09441c73abae77297a09ce6fdc
Michael Bao <mike.h.bao@gmail.com>
2021-01-13 11:09:16 -0500

    fix electron build

--- scripts/windows/package.ps1
+++ scripts/windows/package.ps1
@@ -10,9 +10,12 @@ cmake -S . -B build -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release --target squadov_client_service
 
 Set-Location -Path .\client_ui\common_web
+Remove-Item .\dist\production -Recurse
 yarn run webpack --config ./webpack/$GCP_PROJECT.config.js --env.target=electron-renderer
 
 Set-Location -Path ..\desktop
+Remove-Item .\dist -Recurse
+Remove-Item .\assets -Recurse
 Copy-Item -Path "..\assets" -Destination "assets" -Recurse -Force
 Copy-Item -Path "..\common_web\dist\production\electron-renderer" -Destination dist -Recurse -Force
 yarn run dist --config ..\..\scripts\windows\electron-builder.yml --publish $PUBLISH
