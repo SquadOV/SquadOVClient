ad391c4afcf3cb43ae5f84384f3e4da53b567ec8
Michael Bao <mike.h.bao@gmail.com>
2021-02-23 17:29:04 -0500

    use git commit hash for landing page docker container

--- scripts/build/build_landing.sh
+++ scripts/build/build_landing.sh
@@ -3,8 +3,9 @@ set -xe
 DIR=`dirname ${BASH_SOURCE[0]}`
 ROOTDIR=`realpath ${DIR}/../../`
 GCP_PROJECT=$1
+COMMIT_HASH=`git rev-parse HEAD`
 
 cd "$ROOTDIR/client_ui/landing"
-TAG=registry.gitlab.com/squadov/squadovapiserver/${GCP_PROJECT}/squadov_landing:latest
+TAG=registry.gitlab.com/squadov/squadovapiserver/${GCP_PROJECT}/squadov_landing:${COMMIT_HASH}
 docker build . --tag ${TAG}
 docker push ${TAG}
