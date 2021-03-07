9b364c3f7b538324e1681bfde8eb81ec06ab3723
Michael Bao <mike.h.bao@gmail.com>
2021-03-07 11:08:53 -0500

    use MF as a fallback

--- client_service/service/src/recorder/encoder/ffmpeg_av_encoder.cpp
+++ client_service/service/src/recorder/encoder/ffmpeg_av_encoder.cpp
@@ -320,8 +320,9 @@ void FfmpegAvEncoderImpl::initializeVideoStream(size_t fps, size_t width, size_t
     };
 
     const EncoderChoice encodersToUse[] = {
-        {"h264_amf", VideoStreamContext::GPU },
+        {"h264_mf", VideoStreamContext::CPU },
         {"h264_nvenc", VideoStreamContext::GPU },
+        {"h264_amf", VideoStreamContext::GPU },
         {"libopenh264", VideoStreamContext::CPU }
     };
 
