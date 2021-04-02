be1f3f02d0bd5ccf376f769379f3dabfb2827026
Michael Bao <mike.h.bao@gmail.com>
2021-04-02 13:32:43 -0400

    just need a new commit

--- scripts/build/build_webapp.sh
+++ scripts/build/build_webapp.sh
@@ -8,6 +8,7 @@ COMMIT_HASH=`git rev-parse HEAD`
 cd "$ROOTDIR/client_ui/web/config"
 envsubst < config.json.tmpl > ${GCP_PROJECT}.json
 
+
 cd "$ROOTDIR/client_ui"
 TAG=registry.gitlab.com/squadov/squadovapiserver/${GCP_PROJECT}/squadov_webapp:${COMMIT_HASH}
 docker build . --file Dockerfile.web --tag ${TAG} --build-arg GCP_PROJECT=${GCP_PROJECT}
