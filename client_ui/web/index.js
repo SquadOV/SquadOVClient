03f00b75c661725033d7b616c851233022c4ecd3
Michael Bao <mike.h.bao@gmail.com>
2021-04-02 20:10:23 -0400

    add opengraph and twitter card support to our web app
    fixes #440

--- client_ui/web/index.js
+++ client_ui/web/index.js
@@ -4,6 +4,7 @@ const port = 3000
 const path = require('path')
 const ejs = require('ejs')
 const fs = require('fs')
+const bent = require('bent')
 app.set('view engine', 'ejs');
 app.use('/dist', express.static(__dirname + '/dist'))
 app.use('/assets', express.static(__dirname + '/assets'))
@@ -16,26 +17,43 @@ app.get('/healthz', function (request, response) {
     response.sendStatus(200)
 })
 
-app.get('*', function (request, response) {
+app.get('*', async function (request, response) {
     let apiUrl = parsedConfig.apiUrl
-    let oembedUrl = `${request.protocol}://${request.hostname}${parsedConfig.usePort ? `:${port}` : ''}${request.originalUrl}`
+    let selfUrl = `${request.protocol}://${request.hostname}${parsedConfig.usePort ? `:${port}` : ''}${request.originalUrl}`
     let hasOEmbed = false
+    let hasTwitterCard = false
+    let hasOpenGraph = false
+    let meta = {}
 
-    const url = new URL(oembedUrl)
+    const url = new URL(selfUrl)
     let expectingShare = false
     for (let p of url.pathname.split('/')) {
         if (p === 'share') {
             expectingShare = true
         } else if (expectingShare) {
             hasOEmbed = true
+            hasTwitterCard = true
+            hasOpenGraph = true
+
+            try {
+                const resp = await bent(apiUrl, 'GET', 200)(`/meta?share=${p}`)
+                meta = await resp.json()
+            } catch (e) {
+                console.log('Failed to get share meta for: ', p)
+                hasTwitterCard = false
+                hasOpenGraph = false
+            }
             break
         }
     }
 
     response.render('index', {
-        apiUrl,
-        oembedUrl: encodeURIComponent(oembedUrl),
+        oembedUrl: `${apiUrl}/oembed?url=${encodeURIComponent(selfUrl)}&format=json`,
+        selfUrl,
         hasOEmbed,
+        hasTwitterCard,
+        hasOpenGraph,
+        ...meta,
     })
 })
 
