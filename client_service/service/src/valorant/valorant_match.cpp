86e9693fd8393a89607c8864565f26093e55d8e4
Michael Bao <mike.h.bao@gmail.com>
2021-04-15 11:35:54 -0400

    fix valorant rounds being off when valorant doesn't feel like printing out to the log indicating buy/play
    fixes #533

--- client_service/service/src/valorant/valorant_match.cpp
+++ client_service/service/src/valorant/valorant_match.cpp
@@ -20,6 +20,22 @@ void ValorantMatch::goToRoundState(const shared::TimePoint& tm, shared::valorant
         return;
     }
 
+    if (state == _lastState) {
+        // Some rare situation where VALORANT didn't feel like printing out a statement to demark the start of a round (buy or play).
+        // We can error correct if we're just missing the statement for one event (one buy or one play).
+        if (state == shared::valorant::EValorantRoundState::Buy) {
+            // We skipped the previous play around. We can take a gander at to what the actual time is though since
+            // it's pretty consistently the buy round time + 30.3 seconds.
+            _rounds.back().startTime = _rounds.back().buyTime + std::chrono::milliseconds(30300);
+        } else if (state == shared::valorant::EValorantRoundState::Play) {
+            // We skipped the previous buy round. We can backfill in the previous buy round by subtracting 30.3 seconds (exact opposite of skipping the buy round).
+            ValorantRoundMetadata metadata;
+            metadata.round = _rounds.size();
+            metadata.buyTime = tm - std::chrono::milliseconds(30300);
+            _rounds.push_back(metadata);    
+        }
+    }
+
     if (state == shared::valorant::EValorantRoundState::Buy) {
         ValorantRoundMetadata metadata;
         metadata.round = _rounds.size();
@@ -28,6 +44,7 @@ void ValorantMatch::goToRoundState(const shared::TimePoint& tm, shared::valorant
     } else if (state == shared::valorant::EValorantRoundState::Play && _rounds.size() > 0) {
         _rounds.back().startTime = tm;
     }
+    _lastState = state;
 }
 
 void ValorantMatch::finishMatch(const shared::TimePoint& tm) {
