c69dd21957a9781a0e3d328e8e8ef9fa22a27ebc
Michael Bao <mike@grchive.com>
2021-07-18 14:59:32 -0400

    more stutff to prep migration

--- client_ui/common_web/webpack/staging.config.js
+++ client_ui/common_web/webpack/staging.config.js
@@ -16,7 +16,7 @@ module.exports = (env, argv) => {
         },  
         plugins: [
             new webpack.DefinePlugin({
-                SQUADOV_API_URL: "'https://api.squadov.gg'",
+                SQUADOV_API_URL: "'https://api.staging.squadov.gg'",
                 SQUADOV_USE_ANALYTICS: "true",
             })
         ],
--- scripts/build/build_webapp.sh
+++ scripts/build/build_webapp.sh
@@ -2,14 +2,13 @@
 set -xe
 DIR=`dirname ${BASH_SOURCE[0]}`
 ROOTDIR=`realpath ${DIR}/../../`
-GCP_PROJECT=$1
 COMMIT_HASH=`git rev-parse HEAD`
 
 cd "$ROOTDIR/client_ui/web/config"
-envsubst < config.json.tmpl > ${GCP_PROJECT}.json
+envsubst < config.json.tmpl > ${DEPLOYMENT_ENVIRONMENT}.json
 
 
 cd "$ROOTDIR/client_ui"
-TAG=registry.gitlab.com/squadov/squadovapiserver/${GCP_PROJECT}/squadov_webapp:${COMMIT_HASH}
-docker build . --file Dockerfile.web --tag ${TAG} --build-arg GCP_PROJECT=${GCP_PROJECT}
+TAG=registry.gitlab.com/squadov/squadovapiserver/${DEPLOYMENT_ENVIRONMENT}/squadov_webapp:${COMMIT_HASH}
+docker build . --file Dockerfile.web --tag ${TAG} --build-arg DEPLOYMENT_ENVIRONMENT=${DEPLOYMENT_ENVIRONMENT}
 docker push ${TAG}
