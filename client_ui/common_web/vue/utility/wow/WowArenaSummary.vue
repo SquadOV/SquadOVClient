cdba602eb3f6471e264c9bc5d8f36f3784c3bb05
Michael Bao <mike@grchive.com>
2021-12-08 15:40:17 -0500

    reduce wow match summary clutter by only showing chars who are in the same squad
    fixes #2204

--- client_ui/common_web/vue/utility/wow/WowArenaSummary.vue
+++ client_ui/common_web/vue/utility/wow/WowArenaSummary.vue
@@ -10,6 +10,7 @@
         :disable-link="disableLink"
         :link-to-player-section="linkToPlayerSection"
         :access-token="accessToken"
+        show-full-characters
         @go-to-character="$emit('go-to-character', arguments[0])"
     >
         <template v-slot="{ instanceName }">
