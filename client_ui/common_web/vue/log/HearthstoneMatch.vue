792f775a21c411e043c9535f972c51b0bcc89be6
Michael Bao <mike@grchive.com>
2021-12-20 17:08:38 -0500

    warning when user shares locally recorded vod
    fixes #2389

--- client_ui/common_web/vue/log/HearthstoneMatch.vue
+++ client_ui/common_web/vue/log/HearthstoneMatch.vue
@@ -26,6 +26,7 @@
                         :full-path="$route.fullPath"
                         :timestamp="timestamp"
                         :user-id="userId"
+                        :is-local="vod.isLocal"
                     >
                     </match-share-button>
                 </div>
--- client_ui/common_web/vue/log/LolMatch.vue
+++ client_ui/common_web/vue/log/LolMatch.vue
@@ -23,6 +23,7 @@
                         :full-path="$route.fullPath"
                         :timestamp="timestamp"
                         :user-id="userId"
+                        :is-local="vod.isLocal"
                     >
                     </match-share-button>
                 </div>
