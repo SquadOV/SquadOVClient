46760dbf594c1d66085f21531f46a391460a756a
Michael Bao <mike@grchive.com>
2022-01-09 20:54:37 -0500

    tone down the exposure

--- client_service/service/src/recorder/compositor/compositor.cpp
+++ client_service/service/src/recorder/compositor/compositor.cpp
@@ -177,7 +177,7 @@ void Compositor::reinitOutputTexture(ID3D11Texture2D* input) {
         _tonemapper = std::make_unique<DirectX::ToneMapPostProcess>(_d3dContext->device());
         _tonemapper->SetOperator(DirectX::ToneMapPostProcess::Reinhard);
         _tonemapper->SetTransferFunction(DirectX::ToneMapPostProcess::SRGB);
-        _tonemapper->SetExposure(0.0f);
+        _tonemapper->SetExposure(-1.0f);
     } else {
         _tonemapper.reset();
     }
