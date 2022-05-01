69a97387d395690f6f5c739e767963925180dd15
Jason Lee <leejas@gmail.com>
2022-05-01 01:52:41 -0400

    passes token over in query string for processing

--- client_ui/desktop/login.js
+++ client_ui/desktop/login.js
@@ -5,10 +5,14 @@ const {app, BrowserWindow, ipcMain} = require('electron')
 // The user must type this same password every time they start the app.
 // Note that this is a temporary measure while this is a client-only app -
 // the real goal should be able to use Riot's official APIs.
-module.exports.loginFlow =  function(win) {
+module.exports.loginFlow =  function(win, token) {
     return new Promise((resolve, reject) => {
-        win.loadFile('login.html')
-
+        win.loadFile('login.html',{
+            query: {
+                "googleToken":token
+            }
+        })
+        
         ipcMain.once('finish-login', (event, arg) => {
             resolve()
         })
